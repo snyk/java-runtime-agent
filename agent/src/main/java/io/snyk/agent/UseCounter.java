047be1f4f736ab846e1803e57a011c3384cd1077
Chris West (Faux) <git@goeswhere.com>
2018-07-25 17:19:50 +0100

    totally lost faith in the int[] solution

--- agent/src/main/java/io/snyk/agent/UseCounter.java
+++ agent/src/main/java/io/snyk/agent/UseCounter.java
@@ -1,6 +1,10 @@
 package io.snyk.agent;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 /**
  * An attempt at a fast counter. To be determined if it's actually fast.
@@ -11,28 +15,36 @@ public class UseCounter {
 
     // assumptions: can only grow
     // reassignment @GuardedBy("this")
-    private volatile int[] counters = new int[1024];
+    private volatile AtomicIntegerArray counters = new AtomicIntegerArray(1024);
 
     public synchronized int add(String s) {
         final int id = lookup.size();
         lookup.add(s);
 
-        if (id < counters.length) {
+        final int currentLength = counters.length();
+        if (id < currentLength) {
             return id;
         }
 
+        final AtomicIntegerArray newCounters = new AtomicIntegerArray(currentLength * 2);
+
         // this is also not thread safe, will lose some(tm) updates during the copy
-        counters = Arrays.copyOf(counters, counters.length * 2);
+        for (int i = 0; i < currentLength; i++) {
+            newCounters.set(i, counters.get(i));
+        }
+        counters = newCounters;
 
         return id;
     }
 
     public synchronized Set<String> drain() {
-        int[] old = counters;
-        counters = new int[counters.length];
+        // this is not truly loss-free, but I believe it would be in practice?
+        // the reassignment happens-before we observe any of the values in our backup
+        final AtomicIntegerArray old = counters;
+        counters = new AtomicIntegerArray(counters.length());
         final HashSet<String> ret = new HashSet<>();
-        for (int i = 0; i < old.length; i++) {
-            if (0 != old[i]) {
+        for (int i = 0; i < old.length(); i++) {
+            if (0 != old.get(i)) {
                 ret.add(lookup.get(i));
             }
         }
@@ -40,26 +52,6 @@ public class UseCounter {
     }
 
     public void increment(int id) {
-        // This is NOT thread safe.
-        // We're trying to get some idea of how many times something was called.
-        // TODO: benchmark against AtomicIntArray version
-        // note: there's no AtomicBoolArray. In fact, in hotspot, there's no boolean arrays at all.
-        counters[id] += 1;
+        counters.getAndIncrement(id);
     }
 }
-
-//counters.updateAndGet(current -> {
-//final int current_length = current.length();
-//
-//if (id < current_length) {
-//// someone has beaten us to it
-//return current;
-//}
-//
-//final AtomicIntegerArray newCounters = new AtomicIntegerArray(current_length * 2);
-//for (int i = 0; i < current_length; i++) {
-//newCounters.set(i, current.get(i));
-//}
-//
-//return newCounters;
-//});
