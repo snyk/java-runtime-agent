08e14b53db7502ff5ee5865b30729ebae1cbfbc3
Chris West (Faux) <git@goeswhere.com>
2018-08-16 14:37:30 +0100

    braindead filter parser; deleting

--- agent/src/main/java/io/snyk/agent/logic/Config.java
+++ agent/src/main/java/io/snyk/agent/logic/Config.java
@@ -3,13 +3,16 @@ package io.snyk.agent.logic;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class Config {
     public final String projectId;
-    public final String filter;
+    public final Map<String, Filter> filter;
     public final String urlPrefix;
 
-    Config(String projectId, String filter, String urlPrefix) {
+    Config(String projectId, Map<String, Filter> filter, String urlPrefix) {
         this.projectId = null != projectId ? projectId : "no-project-id-provided";
         this.filter = filter;
         this.urlPrefix = null != urlPrefix ? urlPrefix : "http://localhost:8000";
@@ -25,7 +28,7 @@ public class Config {
 
     private static Config fromLines(Iterable<String> lines) {
         String projectId = null;
-        String filter = null;
+        Map<String, Filter> filters = new HashMap<>();
         String urlPrefix = null;
 
         // this looks awfully like a .properties file. Maybe it could be a .properties file?
@@ -46,21 +49,34 @@ public class Config {
             final String key = splitUp[0];
             final String value = splitUp[1];
 
-            switch (key) {
-                case "projectId":
-                    projectId = value;
-                    break;
-                case "filter":
-                    filter = value;
-                    break;
-                case "urlPrefix":
-                    urlPrefix = value;
-                    break;
-                default:
-                    System.err.println("snyk-agent: unrecognised key: " + key);
+            final String filterName;
+
+            if ("projectId".equals(key)) {
+                projectId = value;
+            } else if ("urlPrefix".equals(key)) {
+                urlPrefix = value;
+            } else if (hasPrefix(key, "filterArtifact", filters, o -> o.artifact = value)) {
+                // TODO: there'll be a better way todo this in the morning, I bet.
+            } else if (hasPrefix(key, "filterVersion", filters, o -> o.version = value)) {
+            } else if (hasPrefix(key, "filterPaths", filters, o -> o.addPathsFrom(value))) {
+            } else {
+                System.err.println("snyk-agent: unrecognised key: " + key);
             }
         }
 
-        return new Config(projectId, filter, urlPrefix);
+        return new Config(projectId, filters, urlPrefix);
+    }
+
+    private static boolean hasPrefix(String from,
+                                     String prefix,
+                                     Map<String, Filter> filters,
+                                     Consumer<Filter> updateFilter) {
+        if (!from.startsWith(prefix)) {
+            return false;
+        }
+
+        updateFilter.accept(filters.computeIfAbsent(from.substring(prefix.length()), name -> new Filter(name)));
+
+        return true;
     }
 }
