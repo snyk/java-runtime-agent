a18fc7a9eeec1820809c3fdf66f4fcb3044f0ccf
Chris West (Faux) <git@goeswhere.com>
2018-08-28 11:08:08 +0100

    eyeball hashCode collisions for guava

--- agent/src/test/java/io/snyk/agent/csum/MethodIdTest.java
+++ agent/src/test/java/io/snyk/agent/csum/MethodIdTest.java
@@ -8,6 +8,10 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.MethodNode;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 
@@ -26,14 +30,42 @@ class MethodIdTest {
         final JarInputStream jar = new JarInputStream(MethodIdTest.class.getResourceAsStream(
                 "/guava-26.0-jre.jar"));
         JarEntry entry;
+        final Map<Integer, Set<String>> seen = new HashMap<>();
+        int running = 0;
+        int totalMethods = 0;
         while (null != (entry = jar.getNextJarEntry())) {
             if (entry.isDirectory() || !entry.getName().endsWith(".class")) {
                 continue;
             }
             for (MethodNode method : AsmUtil.parse(new ClassReader(jar)).methods) {
-                new MethodId().id(method);
+                final int computed = new MethodId().id(method);
+                running += computed;
+                totalMethods += 1;
+                seen.computeIfAbsent(computed, _id -> new HashSet<>())
+                        .add(entry.getName() + " // " + method.name + method.desc);
             }
         }
+
+        int collisions = 0;
+
+        for (Map.Entry<Integer, Set<String>> idNames : seen.entrySet()) {
+            final Set<String> methods = idNames.getValue();
+            if (methods.size() == 1) {
+                continue;
+            }
+
+            collisions += 1;
+
+            System.out.println(idNames.getKey());
+            for (String method : methods) {
+                System.out.println(" * " + method);
+            }
+            System.out.println();
+        }
+
+        assertEquals(15669, totalMethods);
+        assertEquals(505, collisions);
+        assertEquals(-1180639864, running);
     }
 
     private int hashTestMethod(String name) throws IOException {
