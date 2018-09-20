caf1e0bece0c640df760da28f38585af5574aeac
Chris West (Faux) <git@goeswhere.com>
2018-09-20 09:21:03 +0100

    find debian jars which load classes

--- agent/src/test/java/io/snyk/agent/logic/ClassClassifier.java
+++ agent/src/test/java/io/snyk/agent/logic/ClassClassifier.java
@@ -18,6 +18,22 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class ClassClassifier {
+    private static final List<String> STDLIB_CLASS_LOADERS = new ArrayList<>();
+
+    static {
+        STDLIB_CLASS_LOADERS.add("java/lang/ClassLoader");
+        STDLIB_CLASS_LOADERS.add("java/net/URLClassLoader");
+        STDLIB_CLASS_LOADERS.add("java/security/SecureClassLoader");
+        STDLIB_CLASS_LOADERS.add("javax/management/loading/MLet");
+        STDLIB_CLASS_LOADERS.add("javax/management/loading/PrivateMLet");
+        STDLIB_CLASS_LOADERS.add("jdk/nashorn/internal/runtime/NashornLoader");
+        STDLIB_CLASS_LOADERS.add("jdk/nashorn/internal/runtime/ScriptLoader");
+        STDLIB_CLASS_LOADERS.add("jdk/nashorn/internal/runtime/StructureLoader");
+        STDLIB_CLASS_LOADERS.add("java/rmi/server/RMIClassLoader");
+        STDLIB_CLASS_LOADERS.add("javax/xml/stream/FactoryFinder$ClassLoaderFinder");
+        // TODO: can you get AppClassLoader?
+    }
+
     public static void main(String[] args) throws Exception {
         final Multimap<String, File> map = HashMultimap.create();
         Files.walk(Paths.get(args[0]))
@@ -33,15 +49,38 @@ public class ClassClassifier {
                     }
                 }));
 
-        List<String> classLoaders = new ArrayList<>();
-        for (String key : map.keySet()) {
-            if (key.split("#", 2)[0].contains("ClassLoader")) {
-                classLoaders.add(key);
+        for (String knownLoader : STDLIB_CLASS_LOADERS) {
+            final Set<File> libs = new HashSet<>();
+            for (Map.Entry<String, Collection<File>> en : map.asMap().entrySet()) {
+                if (!en.getKey().startsWith(knownLoader)) {
+                    continue;
+                }
+
+                libs.addAll(en.getValue());
             }
+
+            System.out.println(knownLoader);
+            System.out.println("===");
+
+            final ArrayList<File> libsSorted = new ArrayList<>(libs);
+            Collections.sort(libsSorted);
+            for (File lib : libsSorted) {
+                System.out.println(" * " + lib.toString().replaceAll("^deb/[^/]*/", ""));
+            }
+            System.out.println();
         }
-        Collections.sort(classLoaders);
-        for (String classLoader : classLoaders) {
-            System.out.println(classLoader);
+
+        if (false) {
+            List<String> classLoaders = new ArrayList<>();
+            for (String key : map.keySet()) {
+                if (key.split("#", 2)[0].contains("ClassLoader")) {
+                    classLoaders.add(key);
+                }
+            }
+            Collections.sort(classLoaders);
+            for (String classLoader : classLoaders) {
+                System.out.println(classLoader);
+            }
         }
     }
 
@@ -73,6 +112,10 @@ public class ClassClassifier {
                     final AbstractInsnNode insn = insns.next();
                     if (insn instanceof MethodInsnNode) {
                         final MethodInsnNode call = (MethodInsnNode) insn;
+                        if (call.owner.startsWith("[")) {
+                            // we don't need to look at the methods called on "array" (e.g. clone, or... clone)
+                            continue;
+                        }
                         calls.put(call.owner, call.name + call.desc);
                     }
                 }
