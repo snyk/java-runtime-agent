73728cd1e6b0f53d0c625a3e1764d068e0c560f2
Chris West (Faux) <gpg@goeswhere.com>
2019-03-18 10:29:46 +0000

    test: extract mock homebase from python tests

---
+++ e2e/homebase_dir.py
@@ -0,0 +1,35 @@
+import json
+import os
+import tempfile
+from os import path
+from typing import Iterable
+
+
+class HomebaseMock:
+    def __init__(self, config_template: str):
+        self.project_id = '0153525f-5a99-4efe-a84f-454f12494034'
+        self.d = tempfile.TemporaryDirectory()
+        self.config_path = path.join(self.d.name, 'snyk.properties')
+        update_dir = path.join(self.d.name, 'v2', 'snapshot/' + self.project_id)
+        os.makedirs(update_dir)
+        self.update_file = path.join(update_dir, 'java')
+
+        with open(self.config_path, 'w') as f:
+            f.write(config_template
+                    .replace('OUTPUT_PATH', self.d.name)
+                    .replace('PROJECT_ID', self.project_id))
+
+    def write_update(self, update_content: str) -> None:
+        with open(self.update_file, 'w') as f:
+            f.write(update_content)
+        print('>>> update written')
+
+    def all_seen_events(self) -> Iterable:
+        for name in os.listdir(self.d.name):
+            if not name.startswith('post-'):
+                continue
+            name = path.join(self.d.name, name)
+            with open(name) as f:
+                doc = json.load(f)
+            if 'eventsToSend' in doc:
+                yield from doc['eventsToSend']
--- e2e/test-reload.py
+++ e2e/test-reload.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
-import json
 import os
 import re
 import subprocess
-import tempfile
 from os import path
 from time import sleep
-from typing import Iterable
+
+import homebase_dir
 
 
 def main():
-    d = tempfile.TemporaryDirectory()
-    project_id = '0153525f-5a99-4efe-a84f-454f12494033'
-    CONFIG = """
-projectId={}
-homeBaseUrl=file://{}/
+    h = homebase_dir.HomebaseMock("""
+projectId=PROJECT_ID
+homeBaseUrl=file://OUTPUT_PATH/
 logTo=stderr
 
 debugLoggingEnabled=true
@@ -23,17 +20,13 @@ startupDelayMs=10
 filterUpdateIntervalMs=800
 heartBeatIntervalMs=500
 reportIntervalMs=1000
-""".format(project_id, d.name)
-    config_path = path.join(d.name, 'snyk.properties')
-
-    with open(config_path, 'w') as f:
-        f.write(CONFIG)
+""")
 
     victim = subprocess.Popen([
         'java',
         '-javaagent:{}=file://{}'.format(
             path.join(os.getcwd(), 'build/libs/snyk-java-runtime-agent.jar'),
-            config_path
+            h.config_path
         ),
         '-jar',
         'e2e/repeat-action/build/libs/repeat-action.jar'
@@ -47,11 +40,7 @@ reportIntervalMs=1000
     sleep(7.5)
 
     # shouldn't've seen any events yet
-    assert [] == list(all_seen_events(d.name))
-
-    update_dir = path.join(d.name, 'v2', 'snapshot/{}'.format(project_id))
-    os.makedirs(update_dir)
-    update_file = path.join(update_dir, 'java')
+    assert [] == list(h.all_seen_events())
 
     expected_paths = [
         'demo/CalledFromExecutor#run',
@@ -67,15 +56,13 @@ reportIntervalMs=1000
     for i, wanted in enumerate(expected_paths + also_try):
         new_filters += 'filter.up-{}.paths = {}\n'.format(i, wanted)
 
-    with open(update_file, 'w') as f:
-        f.write(new_filters)
-    print('>>> update written to {}'.format(update_file))
+    h.write_update(new_filters)
 
     # test app should run for >2 more seconds, so >4 more reloads and reports
     victim.wait(4)
 
     seen_methods = set()
-    for entry in all_seen_events(d.name):
+    for entry in h.all_seen_events():
         seen_methods.add(re.sub(r'\(.*', '', entry['methodEntry']['methodName']))
 
     print('    seen: {}'.format(sorted(seen_methods)))
@@ -83,17 +70,6 @@ reportIntervalMs=1000
     assert seen_methods == set(expected_paths)
 
     print('Success!')
-
-
-def all_seen_events(out_dir: str) -> Iterable:
-    for name in os.listdir(out_dir):
-        if not name.startswith('post-'):
-            continue
-        name = path.join(out_dir, name)
-        with open(name) as f:
-            doc = json.load(f)
-        if 'eventsToSend' in doc:
-            yield from doc['eventsToSend']
 
 
 if '__main__' == __name__:
